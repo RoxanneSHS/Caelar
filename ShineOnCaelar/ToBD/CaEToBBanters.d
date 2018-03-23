61a9e20789b5347ed6ba0e6abe197f688f670b43
RoxanneSHS <cwillenbockel@aol.com>
2018-03-23 21:20:43 +0100

    v 0.6

--- ShineOnCaelar/ToBD/CaEToBBanters.d
+++ ShineOnCaelar/ToBD/CaEToBBanters.d
@@ -23,3 +23,12 @@ DO~SetGlobal("CealarSarevok","Locals",4)~
 ==BDCAelar~The essence of a god is not tainted, it's pure energy and power. What it will be is up to the decision of the one who has a right to rule it.~
 =~We must assure that this one will be <CHARNAME>.~
 ==BSarev25~Guess what, Caelar, we have some agreement here. The power belongs to <PRO_HIMHER>. The Five and whoever else stands in our way, is common foe. Once we've taken that step, we'll see which of us is right and what will be <CHARNAME>'s decision~EXIT
+
+CHAIN
+IF~Global("CealarImoen","Locals",1)~THEN BDCAelar Imoenkill
+~I heard about your incident at the Ducal Palace recently. We - the crusade - never intended to hurt you.~
+DO~SetGlobal("CealarImoen","Locals",2)~
+==BIMOEN25~Yeah, that's the same story always, people aiming at <CHARNAME> and hittin' me. Must be I'm kinda magnetizing or such.~
+==BDCAelar~I never wanted you or <CHARNAME> dead. I needed <PRO_HIMHER> alive. The men I sent to Baldur's Gate - good and righteous men - were meant to capture <PRO_HIMHER>. We did not know about you at all.~
+==BIMOEN25~Guess what? I know that already. It's that thing about your blood being too thin 'cause that celestial wasn't your direct daddy like our good ol' Bhaal to us. Bet you haven't got those dreams and all the nice extras we have.~
+==BDCAelar~I have the heritage of the Argents, Imoen. This means something to me. For you, the blood is but a curse - for me it is an obligation and I am proud of it.~EXIT
